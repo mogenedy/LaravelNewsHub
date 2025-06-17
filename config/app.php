34007dd891ad8aabb4db8fb41412d18cfe7a5e7d
Ahmed genedy <135667512+mogenedy@users.noreply.github.com>
2025-06-17 15:27:12 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -164,12 +164,12 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
+        // App\Providers\AuthServiceProvider::class,
+        // App\Providers\BroadcastServiceProvider::class,
+        // App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\CheckSettingProvider::class,
-        App\Providers\CasheServiceProvider::class,
+        // App\Providers\CheckSettingProvider::class,
+        // App\Providers\CasheServiceProvider::class,
         App\Providers\ViewServiceProvider::class,
     ])->toArray(),
 
